3137e0bf897bb79428ae07918bd6f9540867cc00
Aaron Turner <aaron@aaronthedev.com>
2020-05-15 18:07:43 -0700

    Continued porting code

--- std/assembly/regexp.ts
+++ std/assembly/regexp.ts
@@ -4,6 +4,47 @@ import {
   case_conv_table1
 } from "./libunicode-qjs";
 
+
+enum REOPCodeEnum {
+  // #define DEF(id, size) REOP_ ## id,
+  // #include "libregexp-opcode.h"
+  // #undef DEF
+  REOP_COUNT,
+}
+
+const CAPTURE_COUNT_MAX = 255;
+const STACK_SIZE_MAX = 255;
+
+/* unicode code points */
+const CP_LS = 0x2028;
+const CP_PS = 0x2029;
+
+const TMP_BUF_SIZE = 128;
+
+type DynBuf = Array<u8>;
+
+class REParseState {
+  byte_code: DynBuf;
+  buf_ptr: u32; // const uint8_t *buf_ptr;
+  buf_end: u32; // const uint8_t *buf_end;
+  buf_start: u32; // const uint8_t *buf_start;
+  re_flags: i32; // int re_flags;
+  is_utf16: boolean; // BOOL is_utf16;
+  ignore_case: boolean; // BOOL ignore_case;
+  dotall: boolean; // BOOL dotall;
+  capture_count: i32; // int capture_count;
+  total_capture_count: i32; // int total_capture_count; /* -1 = not computed yet */
+  has_named_captures: i32; // int has_named_captures; /* -1 = don't know, 0 = no, 1 = yes */
+  mem_opaque: u32; // void *mem_opaque;
+  group_names: DynBuf; // DynBuf group_names;
+  /*
+  union {
+    char error_msg[TMP_BUF_SIZE];
+    char tmp_buf[TMP_BUF_SIZE];
+  } u;
+  */
+}
+
 function is_digit(charCode: i32): boolean {
   return charCode >= "0".charCodeAt(0) && charCode <= "9".charCodeAt(0); 
 }
@@ -93,7 +134,7 @@ const lre_id_continue_table_ascii: Array<u32> = [
 ];
 
 
-const uint16_t char_range_w: Array<u32> = [
+const char_range_w: Array<u16> = [
   4,
   0x0030, 0x0039 + 1,
   0x0041, 0x005A + 1,
@@ -101,8 +142,46 @@ const uint16_t char_range_w: Array<u32> = [
   0x0061, 0x007A + 1,
 ];
 
-// TODO: #define CLASS_RANGE_BASE 0x40000000
-// That line and onward
+const CLASS_RANGE_BASE = 0x40000000
+
+enum CharRangeEnum {
+  CHAR_RANGE_d,
+  CHAR_RANGE_D,
+  CHAR_RANGE_s,
+  CHAR_RANGE_S,
+  CHAR_RANGE_w,
+  CHAR_RANGE_W,
+};
+
+const char_range_table: Array<Array<u16>> = [
+  char_range_d,
+  char_range_s,
+  char_range_w
+];
+
+// TODO: CLean up this function, was working on the REParseState
+function cr_init_char_range(REParseState *s, CharRange *cr, uint32_t c): i32 {
+  boolean invert;
+  const uint16_t *c_pt;
+  int len, i;
+
+  invert = c & 1;
+  c_pt = char_range_table[c >> 1];
+  len = *c_pt++;
+  cr_init(cr, s->mem_opaque, lre_realloc);
+  for(i = 0; i < len * 2; i++) {
+    if (cr_add_point(cr, c_pt[i]))
+      goto fail;
+  }
+  if (invert) {
+    if (cr_invert(cr))
+      goto fail;
+  }
+  return 0;
+  fail:
+    cr_free(cr);
+  return -1;
+}
 
 export class RegExp {
 
